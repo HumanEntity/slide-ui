59dbf8dd548cb191c9e20bc0e750deb23fb20cee
HumanEntity <human_entity@icloud.com>
2023-04-23 19:59:28 +0200

    Added buils script
    Added build script to allow creation of config dir and config file in
    '$HOME/.config/sui/config.toml'.

---
+++ build.rs
@@ -0,0 +1,37 @@
+#![feature(fs_try_exists)]
+
+use std::path::PathBuf;
+use eyre::Result;
+
+fn main() -> Result<()>{
+    #[cfg(feature = "config")]
+    {
+        let home = std::env!("HOME");
+        let config_dir = ".config/sui";
+        if !std::fs::try_exists({
+            let mut path = PathBuf::new();
+            path.push(home);
+            path.push(config_dir);
+            path
+        })? {
+            let user_relative_path = ".config/sui/config.toml";
+
+            let mut config_path = PathBuf::new();
+            config_path.push(home);
+            config_path.push(user_relative_path);
+
+            let os_conf = config_path.clone().into_os_string();
+
+            save_config(&os_conf, "\n")?;
+        }
+    }
+
+    Ok(())
+}
+
+fn save_config(config_path: &std::ffi::OsStr, config_content: &str) -> Result<()> {
+    let parent = std::path::Path::new(&config_path).parent().unwrap();
+    std::fs::create_dir_all(parent)?;
+    std::fs::write(&config_path, config_content)?;
+    Ok(())
+}
