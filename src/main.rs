54fb6fdb8082baeab34db9f896154ccc87e3631b
HumanEntity <human_entity@icloud.com>
2023-05-22 09:00:10 +0200

    Updated main.rs for tests to pass

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
-use crate::{parser::MdParser, render::Renderer};
-use crossterm::Result;
+use crate::{parser::Parser, render::Renderer};
+use config::get_config;
+use eyre::Result;
 
 pub mod cli;
+pub mod config;
 pub mod event;
+pub mod lexer;
 pub mod parser;
 pub mod render;
 
 fn main() -> Result<()> {
     let args = cli::get_args();
+
+    println!("{:?}", get_config()?);
 
     cli::manage_atributes(cli::separate(args.clone()).0);
 
@@ -18,8 +23,10 @@ fn main() -> Result<()> {
 
     let content = cli::read_file(cli::separate(args).1[1].as_str())?;
 
-    let mut parser = MdParser::new(content);
+    let mut parser = Parser::new(content);
     let presentation = parser.parse();
+
+    println!("{presentation:?}");
 
     let mut renderer = Renderer::new(presentation)?;
 
