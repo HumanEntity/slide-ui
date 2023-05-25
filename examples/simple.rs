b2a0d6747c61da9b2ad182cbc513815b86fefaa2
HumanEntity <human_entity@icloud.com>
2023-05-25 11:47:57 +0200

    Updated example to be working

--- examples/simple.rs
+++ examples/simple.rs
@@ -4,7 +4,7 @@ use eyre::Result;
 
 fn main() -> Result<()> {
     let content = include_str!("test.md").to_string();
-    let mut parser = Parser::new(content);
+    let mut parser = Parser::new(content, toml::Table::new());
     let presentation = parser.parse();
     println!("{:?}", presentation);
 
