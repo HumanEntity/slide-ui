949b1c72912f9e560f466f64960da85eaca333a8
HumanEntity <human_entity@icloud.com>
2023-12-19 19:07:49 +0100

    Made main.rs support new API

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -10,3 +10,21 @@ pub trait Lexer {
     fn set_src(&mut self, src: &str);
 }
 
+impl dyn Lexer {
+    fn tokn(&mut self) -> Vec<Token> {
+	self.tokenize()
+    }
+    fn set_source(&mut self, src: &str){
+	self.set_src(src)
+    }
+}
+
+impl Lexer for Box<dyn Lexer>{
+    fn tokenize(&mut self) -> Vec<Token> {
+	self.tokn()
+    }
+    fn set_src(&mut self, src: &str) {
+	self.set_source(src)
+    }
+}
+
